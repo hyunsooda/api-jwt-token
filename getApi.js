2aaeab2201a2deff64fa8b8c2ad56fea8cbc7525
hyunsooda <hyunsoo7708@gmail.com>
2018-12-07 16:54:13 +0900

    Update getApi.js

--- getApi.js
+++ getApi.js
@@ -11,5 +11,4 @@ async function getApiWithToken(token, url, callback) {
 }
 
 
-getApiWithToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imh5dW5zb28iLCJwc3dkIjoiMTIzIiwiaWF0IjoxNTQ0MTY3NzUwLCJleHAiOjE1NDQzNDA1NTB9.kVwG8aUecg22glj8SEI-VBZIbeEY-soybscz94IDLQU',
-'http://localhost:3000/api/currenttime', result => console.log(result));
+getApiWithToken('your token', 'url', 'your callback');
